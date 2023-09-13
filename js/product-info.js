cbd1273f496aed1b1025752f88034b8468eab334
Tomas Pons <t1999pons@gmail.com>
2023-09-13 14:53:54 -0300

    punto4

--- js/product-info.js
+++ js/product-info.js
@@ -1,26 +1,64 @@
 //Inicio Agregado
-const listaEstrellas = [...document.getElementsByClassName("fa-star")];         // Traigo del html una lista con las 5 estrellas
-let posicion;
 
-function pintarEstrellas() {       
+const formulario = document.getElementById('formulario');
+const lista = document.getElementById('lista');
+const btn = document.getElementById('btn');
+const listaEstrellas = [...document.getElementsByClassName("fa-star")];
+
+
+formulario.addEventListener('submit', e => {
+    e.preventDefault()
+    // const puntuacion = document.getElementById('puntuacion').value;
+    const opinion = document.getElementById('opinion').value;
+
+    // let contador = 0;
+    // let starClass = "";
+
+    // for (let i = 0; i < puntuacion; i++) {
+    //     starClass += '<span class="fa fa-star checked"></span>';
+    //     contador++;
+    // }
+    // for (let i = contador; i < 5; i++) {
+    //     starClass += '<span class="fa fa-star"></span>';
+    // }
+
+    lista.innerHTML += `<li class="list-group-item">
+    ${opinion} 
+    </li>`
+
+})
+
+function pintarEstrellas() {
     const pintada = "fa fa-star checked";
     const vacia = "fa fa-star";
     let i;
 
-    listaEstrellas.map((estrella) => {                       // Uso map para iterar sobre los elementos del array (cada estrella) y modificarlos (pintarlos)
-        estrella.addEventListener('click', () => {           // Escucho el click sobre las estrellas vacias
-            i = listaEstrellas.indexOf(estrella);            // Le asigno a i el numero de lugar en el que se encuentra la estrella que se clickeo
-    
-            if (estrella.className===vacia) {                                   // Condicional que calcula cuntas estrellas pintar asignandole la clase correspondiente
-                for (i; i >= 0; --i) listaEstrellas[i].className = pintada;
-            } else {
-                for (i; i < listaEstrellas.length; ++i) listaEstrellas[i].className = vacia;
+    listaEstrellas.map((estrella) => {
+
+        estrella.addEventListener('click', () => {
+            i = listaEstrellas.indexOf(estrella)
+
+            if (estrella.className === vacia) {
+                for (i; i >= 0; i--) {
+                    listaEstrellas[i].className = pintada;                  
+                };
             }
-        });   
+            else {
+                for (i; i < listaEstrellas.length; i++) {
+                    listaEstrellas[i].className = vacia;
+                };
+            };
+        });
     });
 };
 
-pintarEstrellas();
+pintarEstrellas()
+
 
 
 //Fin Agregado
+
+
+
+
+
